ecc990bd85c2804c5319738730ae744b76f8a05a
Ricardo Lucena <ricardo.lucena@gmail.com>
2023-06-30 10:21:30 -0300

    Commit inicial

---
+++ src/main/java/io/github/ricardolucena/Animal.java
@@ -0,0 +1,5 @@
+package io.github.ricardolucena;
+
+public interface Animal {
+    void fazerBarulho();
+}
---
+++ src/main/java/io/github/ricardolucena/repository/ClientesRepository.java
@@ -0,0 +1,11 @@
+package io.github.ricardolucena.repository;
+
+import io.github.ricardolucena.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+    public void persistir(Cliente cliente){
+        //acessa a base e salvar o cliente
+    }
+}

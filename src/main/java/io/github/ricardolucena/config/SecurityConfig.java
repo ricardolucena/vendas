cbb6a3e0cdae3d11db9249d256be987da4598b44
Ricardo Lucena <ricardo.lucena@gmail.com>
2023-07-09 17:45:52 -0300

    76. Testando a autenticação com cadastro de usuarios

--- src/main/java/io/github/ricardolucena/config/SecurityConfig.java
+++ src/main/java/io/github/ricardolucena/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package io.github.ricardolucena.config;
 
+import io.github.ricardolucena.service.impl.UsuarioServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +14,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UsuarioServiceImpl usuarioService;
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -18,11 +24,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder())
-                .withUser("fulano")
-                .password(passwordEncoder().encode("123"))
-                .roles("USER", "ADMIN");
+            auth
+                .userDetailsService(usuarioService)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Override
@@ -36,8 +40,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .hasAnyRole("USER", "ADMIN")
                     .antMatchers("/api/produtos/**")
                         .hasRole("ADMIN")
+                    .antMatchers(HttpMethod.POST, "/api/usuarios/**")
+                        .permitAll()
                 .and()
-                    .formLogin();
+                    .httpBasic();
         ;
     }
 }
